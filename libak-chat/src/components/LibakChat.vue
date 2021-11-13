a6899035ccbbb5007be2632c65fc344be5f92dfc
Leindfraust <rozer223@gmail.com>
2021-11-13 22:29:52 +0800

    Official build update and minor changes when sending message(prevented default, as it is bugged in mobile disabled attribute)

--- libak-chat/src/components/LibakChat.vue
+++ libak-chat/src/components/LibakChat.vue
@@ -5,43 +5,75 @@
                 <ul class="menu-list">
                     <div class="container subtitle" style="padding: 5px">
                         <li>
-                            <button class="dropdown-item button" @click="pickRoom(2021)" :disabled="OldRoomID == 2021">Opptemom Prime</button>
+                            <button
+                                class="dropdown-item button"
+                                @click="pickRoom(2021)"
+                                :disabled="OldRoomID == 2021"
+                            >Opptemom Prime</button>
                         </li>
                         <li>
-                            <button class="dropdown-item button" @click="pickRoom(2025)" :disabled="OldRoomID == 2025">Cocomelon</button>
+                            <button
+                                class="dropdown-item button"
+                                @click="pickRoom(2025)"
+                                :disabled="OldRoomID == 2025"
+                            >Cocomelon</button>
                         </li>
                         <li>
-                            <button class="dropdown-item button" @click="pickRoom(2030)" :disabled="OldRoomID == 2030">Flazymama</button>
+                            <button
+                                class="dropdown-item button"
+                                @click="pickRoom(2030)"
+                                :disabled="OldRoomID == 2030"
+                            >Flazymama</button>
                         </li>
                         <li>
-                            <button class="dropdown-item button" @click="pickRoom(2050)" :disabled="OldRoomID == 2050">Red Horse</button>
+                            <button
+                                class="dropdown-item button"
+                                @click="pickRoom(2050)"
+                                :disabled="OldRoomID == 2050"
+                            >Red Horse</button>
                         </li>
                         <li>
-                            <button class="dropdown-item button" @click="pickRoom(2075)" :disabled="OldRoomID == 2075">Only judge can god me</button>
+                            <button
+                                class="dropdown-item button"
+                                @click="pickRoom(2075)"
+                                :disabled="OldRoomID == 2075"
+                            >Only judge can god me</button>
                         </li>
                     </div>
                 </ul>
             </aside>
         </div>
-        <div class="column" v-if="!roomPicked"><h1 class="title has-text-centered" id="pickRoomDialogue" style="margin-top: 50vh">Pick a room to start chatting!</h1></div>
+        <div class="column" v-if="!roomPicked">
+            <h1
+                class="title has-text-centered"
+                id="pickRoomDialogue"
+                style="margin-top: 50vh"
+            >Pick a room to start chatting!</h1>
+        </div>
         <div class="column" v-if="roomPicked">
-            <div class="box" id="chatbox" style="width: 100%; margin: auto; height: 94vh; overflow: auto;">
+            <div
+                class="box"
+                id="chatbox"
+                style="width: 100%; margin: auto; height: 94vh; overflow: auto;"
+            >
                 <div class="container" v-for="(message, index) in messages" :key="index">
                     <div class="has-text-centered">
-                        <span
-                            v-if="message.hostMsg != null"
-                            class="notification has-text-info"
-                        ><b>{{ message.hostMsg }}</b></span>
+                        <span v-if="message.hostMsg != null" class="notification has-text-info">
+                            <b>{{ message.hostMsg }}</b>
+                        </span>
                     </div>
-                    
-                    <div v-if="message.message != null" class="content" >
-                        <h3 ><img :src="message.userpic">
-                            <b>{{ message.user }}: {{ message.message }}</b><br/>
+                    <br />
+
+                    <div v-if="message.message != null" class="content">
+                        <h3>
+                            <img v-bind:src="message.userpic" />
+                            <b>{{ message.user }}: {{ message.message }}</b>
+                            <br />
                         </h3>
                     </div>
                 </div>
             </div>
-            <div class="field has-addons" >
+            <div class="field has-addons">
                 <p class="control" style="width: 100%;">
                     <input
                         type="input"
@@ -54,8 +86,7 @@
                     <button
                         class="button is-info"
                         type="button"
-                        :disabled="input == ''"
-                        @click="sendMessage(this.input)"
+                        @click="sendMessage($event, this.input)"
                     >Send</button>
                 </p>
             </div>
@@ -79,7 +110,7 @@ export default {
                 message: msg,
                 userpic: userpic
             });
-            setTimeout(()=> {this.scrollBottom()}, 100)
+            setTimeout(() => { this.scrollBottom() }, 100)
         });
         socket.on('host message', (hostMsg, roomNo) => {
             this.messages.push({
@@ -94,7 +125,7 @@ export default {
             hostMessage: [],
             currentUser: this.$store.state.username,
             input: '',
-            imgPreUrl: 'https://avatars.dicebear.com/api/micah/',
+            imgPreUrl: 'https://ui-avatars.com/api/?name=',
             extUrl: '.svg',
             roomPicked: false,
             OldRoomID: ''
@@ -108,18 +139,22 @@ export default {
             socket.emit('join room', room, `${this.currentUser} has joined the room.`)
             this.messages = []
             this.roomPicked = true
-            if(this.OldRoomID) {
+            if (this.OldRoomID) {
                 socket.emit('leave room', this.OldRoomID, `${this.currentUser} has left the room.`)
             }
         },
-        sendMessage(msg) {
-            let userpic = this.imgPreUrl + this.currentUser + this.extUrl
-            socket.emit('message', msg, this.currentUser, userpic)
-            this.input = ''
+        sendMessage(e, msg) {
+            let userpic = this.imgPreUrl + this.currentUser
+            if (this.input == '') {
+                e.preventDefault()
+            } else {
+                socket.emit('message', msg, this.currentUser, userpic)
+                this.input = ''
+            }
         },
-        scrollBottom(){
+        scrollBottom() {
             let el = document.getElementById('chatbox')
-            el.lastElementChild.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            el.lastElementChild.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
         }
     }
 }
@@ -129,15 +164,15 @@ export default {
 img {
     width: 5%;
 }
-@media (max-width: 991.98px){
+@media (max-width: 991.98px) {
     img {
-        width: 15%
+        width: 15%;
     }
     #pickRoomDialogue {
-        margin-top: 0px !important
+        margin-top: 0px !important;
     }
     #chatbox {
-        height: 65vh !important
+        height: 65vh !important;
     }
 }
 </style>
