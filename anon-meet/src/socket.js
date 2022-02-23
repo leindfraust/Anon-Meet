37a87b36234437a49a691339ce00a11b680855e1
Leindfraust <rozer223@gmail.com>
2022-02-23 18:51:46 +0800

    Update socket.js

--- anon-meet/src/socket.js
+++ anon-meet/src/socket.js
@@ -1,8 +1,8 @@
 import {
     io
 } from "socket.io-client"
-////const URL = "http://localhost:5000"
-const socket = io({
+const URL = "http://localhost:5000"
+const socket = io(process.env.PORT,{
     autoConnect: false
 });
 
