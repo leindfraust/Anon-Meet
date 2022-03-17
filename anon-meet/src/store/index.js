611c841c5c3eb501b6964bd19d81eea80b83f9ce
Leindfraust <rozer223@gmail.com>
2022-03-17 18:59:09 +0800

    feat: vuex now persists

--- anon-meet/src/store/index.js
+++ anon-meet/src/store/index.js
@@ -1,7 +1,11 @@
 import {
     createStore
 } from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+})
 export const store = createStore({
     state() {
         return {
@@ -12,5 +16,6 @@ export const store = createStore({
         username(state, username) {
             state.username = username
         }
-    }
+    },
+    plugins: [vuexLocal.plugin]
 });
